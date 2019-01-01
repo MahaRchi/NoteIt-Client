7c3044e35c68b659ea5d2d6d325261d85aeb805e
Maha <mahrchi93@gmail.com>
2019-01-01 12:28:01 +0100

    initialisation projet

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { NotesComponent } from './notes/notes.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http"
+const routes: Routes = [
+  { path: 'notes', component: NotesComponent },
+  { path: 'feedback', component: FeedbackComponent },
+  { path: '', component: NotesComponent, pathMatch:'full' },
+  { path: '**', component: NotFoundComponent}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavigationComponent,
+    FeedbackComponent,
+    NotesComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes, {enableTracing :true})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/feedback/feedback.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-feedback',
+  templateUrl: './feedback.component.html',
+  styleUrls: ['./feedback.component.css']
+})
+export class FeedbackComponent implements OnInit {
+
+  model:FeedbackViewModel = {
+    name:'',
+    email:'',
+    feedback:''
+  };
+  constructor(private http: HttpClient) {
+
+   }
+
+  ngOnInit() {
+  }
+
+  sendFeedback(): void {
+    alert(this.model.name);
+  }
+
+}
+
+  export interface FeedbackViewModel{
+
+    name:string;
+    email:string;
+    feedback:string;
+  }
+
+}
