bc7b4aaf93f69bdb8637c09c057419729c8bb80f
Maha Ferchichi <mferchichi@genitechtunisie.softeam.fr>
2019-01-02 17:10:05 +0100

    crud notebbok nd note

--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ApiService} from '../shared/api.service';
 import {Notebook} from './model/notebook';
-
+import {Note} from './model/note';
 @Component({
   selector: 'app-notes',
   templateUrl: './notes.component.html',
@@ -10,22 +10,115 @@ import {Notebook} from './model/notebook';
 export class NotesComponent implements OnInit {
 
 notebooks : Notebook[] = [];
+notes : Note[] = []
+selectedNoteBook : Notebook;
 
   constructor(private apiService: ApiService) { }
 
   ngOnInit() {
 
     this.getAllNotebooks();
+    this.getAllNotes();
   }
 
 public getAllNotebooks()
 {
   this.apiService.getAllNotebooks().subscribe(
 
-    res => {this.notebooks = res},
+    res => {this.notebooks = res;},
     err => {alert('An error was occured')}
   );
 
 }
 
+createNotebook()
+{
+  let newNb: Notebook = {
+    name:'test',
+    id: null,
+    nbNotes: 0
+  }
+
+  this.apiService.postNotebook(newNb).subscribe(
+
+    res => {
+      newNb.id = res.id;
+      this.notebooks.push(newNb);
+    },
+    err => {alert('An error was occured')}
+  );
+
+}
+
+updateNotebook(updatedNbk: Notebook)
+{
+  this.apiService.postNotebook(updatedNbk).subscribe(
+
+    res => {
+      console.log("updated")
+    },
+    err => {alert('An error was occured')}
+  );
+}
+
+deleteNotebook(notebook : Notebook){
+
+  if(confirm("Are u sure u want to delete?")){
+    this.apiService.deleteNotebook(notebook.id).subscribe(
+      res => {
+        let indexOfNotebook = this.notebooks.indexOf(notebook);
+        this.notebooks.splice(indexOfNotebook,1);
+
+      };
+      err => {alert("could not delete");}
+    )
+  }
+}
+
+getAllNotes()
+{
+  this.apiService.getAllNotes().subscribe(
+    res => {this.notes = res;},
+    err => {alert('An error was occured');}
+  )
+}
+
+deleteNote(note : Note)
+{
+
+}
+
+createNote(notebookId : string){
+  let newNote : Note = {
+
+    id: null,
+    title:"New Note",
+    text: "tester le texte",
+    lastModifiedOn: null,
+    notebookId: notebookId
+  };
+
+  this.apiService.postNote(newNote).subscribe(
+    res => {
+      newNote.id = res.id;
+      this.notes.push(newNote);
+
+
+    },
+    err => {alert('An error was occured while saving note');}
+  );
+}
+
+selectNotebook(notebook : Notebook){
+  this.selectedNoteBook = notebook;
+  console.log( this.selectedNoteBook);
+  this.apiService.getNotesByNotebook(this.selectedNoteBook.id).subscribe(
+    res => {
+
+      this.notes=res;
+
+    },
+    err => {alert("eerror while grabing note by notebook");}
+  );
+}
 }
--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import {Notebook} from '../notes/model/notebook';
+import {Note} from '../notes/model/note';
 import {FeedbackViewModel} from '../feedback/feedback.component';
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,12 @@ export class ApiService {
 private BASE_URL = 'http://localhost:8082/api/';
 
 private ALL_NOTEBOOKS_URL = `${this.BASE_URL}/notebooks/all`;
+private SAVE_UPDATE_NOTEBOOK_URL = `${this.BASE_URL}/notebooks`;
 private SEND_FEEDBACK_URL = `${this.BASE_URL}/feedback`;
+private DELETE_NOTEBOOK_URL =`${this.BASE_URL}/notebooks/`;
+private ALL_NOTES_URL =`${this.BASE_URL}/notes/all`;
+private SAVE_UPDATE_NOTE_URL =`${this.BASE_URL}/notes`;
+private NOTES_BYNOTEBOOK_URL =`${this.BASE_URL}\\notes\\byNotebook\\`;
 
   constructor(private http : HttpClient) {}
 
@@ -19,9 +25,37 @@ getAllNotebooks(): Observable<Notebook[]>
   return this.http.get<Notebook[]>(this.ALL_NOTEBOOKS_URL);
 }
 
+getAllNotes(): Observable<Note[]>
+{
+  return this.http.get<Note[]>(this.ALL_NOTES_URL);
+}
+
+getNotesByNotebook(nbId: String): Observable<Note[]>
+{
+  return this.http.get<Note[]>(this.NOTES_BYNOTEBOOK_URL+nbId);
+}
+
 postFeedback(feedback : FeedbackViewModel) : Observable<any>
 {
   return this.http.post(this.SEND_FEEDBACK_URL,feedback);
 }
+
+postNotebook(nb : Notebook) : Observable<Notebook>
+{
+  return this.http.post(this.SAVE_UPDATE_NOTEBOOK_URL,nb);
+}
+
+postNote(note : Note) : Observable<Note>
+{
+  return this.http.post<Note>(this.SAVE_UPDATE_NOTE_URL,note);
+}
+
+deleteNotebook(id: String): Observable<any>
+{
+  return this.http.delete(this.DELETE_NOTEBOOK_URL + id);
+}
+
+
+
 
 }
